21b00792bbb02ff7f26f612954f53c33aa0e6405
ymski <114902604+ymski@users.noreply.github.com>
2024-01-22 19:59:19 +0900

    handling address duplication with multiple host data
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>

--- src/caret_analyze/infra/lttng/lttng.py
+++ src/caret_analyze/infra/lttng/lttng.py
@@ -28,6 +28,7 @@ import pandas as pd
 from tqdm import tqdm
 
 from .events_factory import EventsFactory
+from .id_remapper import AddressRemapper
 from .lttng_event_filter import LttngEventFilter, SameAddressFilter
 from .ros2_tracing.data_model import Ros2DataModel
 from .ros2_tracing.data_model_service import DataModelService
@@ -428,6 +429,7 @@ class Lttng(InfraBase):
         if isinstance(trace_dir_or_events[0], str):
             tid_remapper = MultiHostIdRemapper(LttngEventFilter.VTID)
             pid_remapper = MultiHostIdRemapper(LttngEventFilter.VPID)
+            evant_remapper = AddressRemapper()
             for trace_dir in trace_dir_or_events:
                 event_collection = EventCollection(
                     trace_dir, force_conversion)  # type: ignore
@@ -445,7 +447,7 @@ class Lttng(InfraBase):
                         offset = Ros2Handler.get_monotonic_to_system_offset(event)
                         break
 
-                handler = Ros2Handler(data, offset)
+                handler = Ros2Handler(data, evant_remapper, offset)
 
                 init_events = []
                 run_events = []
@@ -516,7 +518,8 @@ class Lttng(InfraBase):
                     offset = Ros2Handler.get_monotonic_to_system_offset(event)
                     break
 
-            handler = Ros2Handler(data, offset)
+            event_remapper = AddressRemapper()
+            handler = Ros2Handler(data, event_remapper, offset)
 
             begin = events[0][LttngEventFilter.TIMESTAMP]
             end = events[-1][LttngEventFilter.TIMESTAMP]
